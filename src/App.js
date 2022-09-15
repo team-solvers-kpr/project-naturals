60d3ade28315e9692eeb5f992bd46348a38624c5
unknown <xahin.mahir@gmail.com>
2022-09-15 17:42:15 +0600

    nav and home banner done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/pages/Home/Home'
+import Navbar from './components/shared/NavBar/Navbar';
 
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
     </div>
   );
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import HelpPosts from "./HelpPosts/HelpPosts/HelpPosts";
+import HomeBanners from "./HomeBanners/HomeBanners/HomeBanners";
 import Reviews from "./Reviews/Reviews/Reviews";
 
 const Home = () => {
   return (
     <div>
+      <HomeBanners />
       <Reviews />
       <HelpPosts />
     </div>
