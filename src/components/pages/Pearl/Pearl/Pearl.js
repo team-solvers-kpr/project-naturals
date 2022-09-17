122e6d1559d0e1535370496cd72aa4b05546f2a6
unknown <xahin.mahir@gmail.com>
2022-09-17 17:46:59 +0600

    Pearl Bottom Banner Done

--- src/components/pages/Pearl/Pearl/Pearl.js
+++ src/components/pages/Pearl/Pearl/Pearl.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BottomPearlBanner from '../BottomPearlBanner/BottomPearlBanner';
 import PearlBanner from '../PearlBanner/PearlBanner';
 
 const Pearl = () => {
     return (
         <div>
             <PearlBanner />
+            <BottomPearlBanner />
         </div>
     );
 };
